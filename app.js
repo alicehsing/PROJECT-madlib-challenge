36df576cf292ac3c11e404bf348fdfa487118326
armaBurton <armanarma@armaburton.com>
2021-10-08 14:12:28 -0700

    intialize variables

--- app.js
+++ app.js
@@ -9,6 +9,62 @@
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
 
-function madLib(input, output) {
-    const pluralNoun1 = document.getElementById(`${input}-input`);
+function madLib(input) {
+    const pluralNoun1 = document.getElementById(`${input}-input`).value;
     const adjective1 = document.getElementById(`${input}-input`);
+    const pluralNounAnimal1 = document.getElementById(`${input}-input`);
+    const pluralNoun2 = document.getElementById(`${input}-input`);
+    const adjective2 = document.getElementById(`${input}-input`);
+    const color = document.getElementById(`${input}-input`);
+    const adjective3 = document.getElementById(`${input}-input`);
+    const noun1 = document.getElementById(`${input}-input`);
+    const pluralNoun3 = document.getElementById(`${input}-input`);
+    const adjective4 = document.getElementById(`${input}-input`);
+    const verb1 = document.getElementById(`${input}-input`);
+    const pluralNoun4 = document.getElementById(`${input}-input`);
+    const verbEd1 = document.getElementById(`${input}-input`);
+    const verb2 = document.getElementById(`${input}-input`);
+    const noun2 = document.getElementById(`${input}-input`);
+    const adjective5 = document.getElementById(`${input}-input`);
+    
+    const pluralNoun1Out = document.getElementById(`${input}`);
+    const adjective1Out = document.getElementById(`${input}`);
+    const pluralNounAnimal1Out = document.getElementById(`${input}`);
+    const pluralNoun2Out = document.getElementById(`${input}`);
+    const adjective2Out = document.getElementById(`${input}`);
+    const colorOut = document.getElementById(`${input}`);
+    const adjective3Out = document.getElementById(`${input}`);
+    const noun1Out = document.getElementById(`${input}`);
+    const pluralNoun3Out = document.getElementById(`${input}`);
+    const adjective4Out = document.getElementById(`${input}`);
+    const verb1Out = document.getElementById(`${input}`);
+    const pluralNoun4Out = document.getElementById(`${input}`);
+    const verbEd1Out = document.getElementById(`${input}`);
+    const verb2Out = document.getElementById(`${input}`);
+    const noun2Out = document.getElementById(`${input}`);
+    const adjective5Out = document.getElementById(`${input}`);
+
+
+
+}
+
+function madLibify ('click', () =>{
+    
+})
+
+madLib(`p-noun-01`);
+madLib(`adj-01`);
+madLib(`p-noun-animal-01`);
+madLib(`p-noun-02`);
+madLib(`adj-02`);
+madLib(`color`);
+madLib(`adj-03`);
+madLib(`noun-01`);
+madLib(`p-noun-03`);
+madLib(`adj-04`);
+madLib(`verb-01`);
+madLib(`p-noun-04`);
+madLib(`verb-ed-01`);
+madLib(`verb-02`);
+madLib(`noun-02`);
+madLib(`adj-05`);
