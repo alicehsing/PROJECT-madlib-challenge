7a455ae313e74f4950c9aecddfd9c5c7714ddd4d
ClareMcDonald <clare.s.mcdonald@gmail.com>
2021-10-08 13:54:20 -0700

    added js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@
 
 
 // set event listeners 
+
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
+function madLib(input, output) {
+    const pluralNoun1 = document.getElementById(`${input}-input`);
+    const adjective1 = document.getElementById(`${input}-input`);
